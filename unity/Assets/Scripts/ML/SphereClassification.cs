56f4c7c68b77039bb00ef5ad619affd3c31ce3d9
Mohamed <mohamedali.kaddour@gmail.com>
2019-10-22 10:30:42 +0200

    Update Scene + .gitIgnore

---
+++ unity/Assets/Scripts/ML/SphereClassification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SphereClassification : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("begin");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var model = ml_toolbox.linear_create_model(5);
+            var input = new Double[5];
+            var res = ml_toolbox.linear_classify(model, input, 5);
+            Debug.Log(res);
+            ml_toolbox.linear_remove_model(model); 
+        }
+            
+        
+        
+    }
+}
