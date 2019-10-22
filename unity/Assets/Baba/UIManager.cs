63bccb278b7248a88e4a036eadd25dc58bff7028
Bastien PERROTEAU <bastienperoteau@gmail.com>
2019-10-22 11:14:23 +0200

    UiFinalSet

---
+++ unity/Assets/Baba/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text TxtSphere;
+    public Text TxtColor;
+
+    public void SetNbSphere(Slider val)
+    {
+        this.GetComponent<MLClassification>().NbSphere = (int)val.value;
+        TxtSphere.text = "Sphère : " + val;
+    }
+    public void SetNbColor(Slider val)
+    {
+        this.GetComponent<MLClassification>().NbColor = (int)val.value;
+        TxtColor.text = "Couleurs : " + val;
+    }
+}
