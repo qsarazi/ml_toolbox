dc8838e514e38f9de15e65806d4b4e1238e8221c
Quentin Sarazi <quentin.sarazi@gmail.com>
2019-10-22 19:25:33 +0200

    mlp added, not completly implmented yet

---
+++ ml_toolbox/ml_toolbox/Mlp.h
@@ -0,0 +1,20 @@
+#pragma once
+
+extern "C" {
+
+	struct Mlp {
+		double ***weights; //1 : layer, 2 : layer size, 3 : next layer size
+		double **outputs;
+		double **sums;
+		double **deltas;
+		int layerCount;
+		int *npl; // neurons per layer
+	};
+
+	__declspec(dllexport) Mlp *mlp_create_model(int layerCount, int *npl);
+	__declspec(dllexport) void mlp_remove_model(Mlp *model);
+	__declspec(dllexport) int mlp_fit_regression(Mlp *model, double *inputs, int inputSize /**/);
+	__declspec(dllexport) int mlp_fit_classification(Mlp *model, double *inputs, int inputSize /**/);
+	__declspec(dllexport) double *mlp_predict(Mlp *model, double *inputs, int inputSize);
+	__declspec(dllexport) double *mlp_classify(Mlp *model, double *inputs, int inputSize);
+}
