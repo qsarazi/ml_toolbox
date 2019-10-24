38d8d6c4f7096e374069d557c54ea2554f449cf9
Bastien PERROTEAU <bastienperoteau@gmail.com>
2019-10-24 13:04:07 +0200

    Commit new scene

--- unity/Assets/Baba/MLClassification.cs
+++ unity/Assets/Baba/MLClassification.cs
@@ -15,6 +15,8 @@ public class MLClassification : MonoBehaviour
 
     public Dropdown Ddown;
 
+    public GameObject BackField;
+
     public int NbSphere = 3;
 
     public int NbColor = 2;
@@ -29,9 +31,16 @@ public class MLClassification : MonoBehaviour
 
     private bool ModelCreated = false;
 
-    private float MaxZ = 24;
+    private float MaxX = 24;
 
     private IntPtr myModel;
+
+    private float ZSphere = 10f;
+
+    void Start()
+    {
+        InitBAckField();
+    }
 
     void Update()
     {
@@ -40,6 +49,27 @@ public class MLClassification : MonoBehaviour
         {
             Application.Quit();
         }
+    }
+
+    void InitBAckField()
+    {
+        for (int x = (int) -MaxX+1; x < MaxX; x++)
+        {
+            for (int y = -15; y < 15; y++)
+            {
+                InitImg(x, y);
+            }
+        }
+    }
+
+    public void InitImg(int x, int y)
+    {
+        GameObject Go = new GameObject();
+        Go.transform.SetParent(BackField.transform);
+        Go.AddComponent<Image>().color = Color.gray;
+        Go.GetComponent<RectTransform>().sizeDelta = new Vector2(2f, 2f);
+        Go.transform.position = BackField.transform.position;
+        Go.GetComponent<RectTransform>().localPosition += new Vector3(20*x, 20*y,0);
     }
 
     public void Init()
@@ -63,7 +93,6 @@ public class MLClassification : MonoBehaviour
                 break;
             case 5:
                 break;
-
         }
     }
 
@@ -76,7 +105,7 @@ public class MLClassification : MonoBehaviour
             double[] input = new Double[2];
             // Remplir Input
             input[0] = (double)child.position.y;
-            input[1] = (double)child.position.z;
+            input[1] = (double)child.position.x;
             // Calcul résultat
             double res = ml_toolbox.linear_classify(myModel, input, 2);
             // Affichage Debug
@@ -141,7 +170,7 @@ public class MLClassification : MonoBehaviour
                 double[] input = new Double[2];
                 // Remplir Input
                 input[0] = (double)child.position.y;
-                input[1] = (double)child.position.z;
+                input[1] = (double)child.position.x;
                 // Calcul résultat
                 double res = ml_toolbox.linear_classify(myModel, input, 2);
                 // Vérifie et Fit
@@ -150,6 +179,78 @@ public class MLClassification : MonoBehaviour
         }
         // Lance un test de l'IA
         LaunchIAResult();
+    }
+
+    public void LaunchIAResult2()
+    {
+        // On regarde toutes les sphères
+        foreach (Transform child in BackField.transform)
+        {
+            // Créer Input
+            double[] input = new Double[2];
+            // Remplir Input
+            input[1] = (double)child.position.y/2;
+            input[0] = (double)child.position.x/2;
+            
+            // Calcul résultat
+            double res = ml_toolbox.linear_classify(myModel, input, 2);
+            // Affichage Debug
+            Debug.Log(child.name + " : " + input[0] + "/" + input[1] + "/" + res);
+            switch (res)
+            {
+                case -1:
+                    child.gameObject.GetComponent<Image>().color = Color.blue;
+                    break;
+                case 1:
+                    child.gameObject.GetComponent<Image>().color = Color.red;
+                    break;
+            }
+        }
+    }
+
+    public void TrainIA2()
+    {
+        foreach (Transform child in this.transform)
+        {
+            Debug.Log(child.name + " : " + child.position.x + "/" + child.position.y);
+        }
+        // On recréait le model si pas de model crée
+        if (!ModelCreated) myModel = ml_toolbox.linear_create_model(2);
+        ModelCreated = true;
+        // Boucle d'entrainement entrainements
+        for (int cpt = 0; cpt < NbTraining; cpt++)
+        {
+            // On regarde toutes les sphères
+            foreach (Transform child in this.transform)
+            {
+                // Set expected value suvant couleur
+                double expectedvalue = 0;
+                switch (child.gameObject.tag)
+                {
+                    case "Blue":
+                        expectedvalue = -1;
+                        break;
+                    case "Red":
+                        expectedvalue = 1;
+                        break;
+                    case "Green":
+                        expectedvalue = 0;
+                        break;
+                }
+                // Créer Input
+                double[] input = new Double[2];
+                // Remplir Input
+                input[1] = (double)child.position.y;
+                input[0] = (double)child.position.x;
+                //Debug.Log(child.name + " : " + input[0] + "/" + input[1]);
+                // Calcul résultat
+                double res = ml_toolbox.linear_classify(myModel, input, 2);
+                // Vérifie et Fit
+                ml_toolbox.linear_fit_classification(myModel, input, 2, Pas, expectedvalue, res);
+            }
+        }
+        // Lance un test de l'IA
+        LaunchIAResult2();
     }
 
     public void RemoveModel()
@@ -216,8 +317,8 @@ public class MLClassification : MonoBehaviour
                 GO.tag = "Green";
             }
             // Decalage en Z
-            float Z = -(MaxZ) + ((float)cpt * 2 * (MaxZ + 1) / NbSphere);
-            GO.transform.position = new Vector3(0f, Y, Z);
+            float X = -(MaxX) + ((float)cpt * 2 * (MaxX + 1) / NbSphere);
+            GO.transform.position = new Vector3(X, Y, ZSphere);
         }
     }
 
@@ -271,8 +372,8 @@ public class MLClassification : MonoBehaviour
                 GO.tag = "Green";
             }
             // Decalage en Z
-            float Z = -(MaxZ) + ((float)cpt * 2 * (MaxZ + 1) / NbSphere);
-            GO.transform.position = new Vector3(0f, Y, Z);
+            float X = -(MaxX) + ((float)cpt * 2 * (MaxX + 1) / NbSphere);
+            GO.transform.position = new Vector3(X, Y, ZSphere);
         }
     }
 
@@ -291,19 +392,19 @@ public class MLClassification : MonoBehaviour
                 cptb = 1;
             }
             float Y = 0f;
-            float Z = 0f;
+            float X = 0f;
             if (cptb == 0)
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 if (cpt % 2 == 0)
                 {
-                    Y = UnityEngine.Random.Range(0.5f, MaxZ);
-                    Z = UnityEngine.Random.Range(-MaxZ, -0.5f);
+                    Y = UnityEngine.Random.Range(0.5f, MaxX);
+                    X = UnityEngine.Random.Range(-MaxX, -0.5f);
                 }
                 else
                 {
-                    Y = UnityEngine.Random.Range(-MaxZ, -0.5f);
-                    Z = UnityEngine.Random.Range(0.5f, MaxZ);
+                    Y = UnityEngine.Random.Range(-MaxX, -0.5f);
+                    X = UnityEngine.Random.Range(0.5f, MaxX);
                 }
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
@@ -314,17 +415,17 @@ public class MLClassification : MonoBehaviour
                 if (cpt % 2 == 0)
                 {
                     Y = UnityEngine.Random.Range(-15f, -0.5f);
-                    Z = UnityEngine.Random.Range(-MaxZ, -0.5f);
+                    X = UnityEngine.Random.Range(-MaxX, -0.5f);
                 }
                 else
                 {
                     Y = UnityEngine.Random.Range(0.5f, 15f);
-                    Z = UnityEngine.Random.Range(0.5f, MaxZ);
+                    X = UnityEngine.Random.Range(0.5f, MaxX);
                 }
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(X, Y, ZSphere);
         }
     }
 
@@ -339,14 +440,14 @@ public class MLClassification : MonoBehaviour
             // Making Color
             int cptb = UnityEngine.Random.Range(0, NbColor);
             float Y = 0f;
-            float Z = 0f;
+            float X = 0f;
             if (cptb == 0)
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 if(UnityEngine.Random.Range(0, 2) == 1)Y = UnityEngine.Random.Range(-15f, -14f);
                 else Y = UnityEngine.Random.Range(14f, 15f);
-                if (UnityEngine.Random.Range(0, 2) == 1) Z = UnityEngine.Random.Range(-MaxZ, -MaxZ+1f);
-                else Z = UnityEngine.Random.Range(MaxZ-1f, MaxZ);
+                if (UnityEngine.Random.Range(0, 2) == 1) X = UnityEngine.Random.Range(-MaxX, -MaxX+1f);
+                else X = UnityEngine.Random.Range(MaxX-1f, MaxX);
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
             }
@@ -354,21 +455,21 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Red;
                 Y = UnityEngine.Random.Range(-15f, 15f);
-                Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
-                while (Z < -MaxZ+1 && (Y < -14f || Y > 14f))
+                X = UnityEngine.Random.Range(-MaxX, MaxX);
+                while (X < -MaxX+1 && (Y < -14f || Y > 14f))
                 {
                     Y = UnityEngine.Random.Range(-15f, 15f);
-                    Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
+                    X = UnityEngine.Random.Range(-MaxX, MaxX);
                 }
-                while (Z > MaxZ-1 && (Y < -14f || Y > 14f))
+                while (X > MaxX-1 && (Y < -14f || Y > 14f))
                 {
                     Y = UnityEngine.Random.Range(-15f, 15f);
-                    Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
+                    X = UnityEngine.Random.Range(-MaxX, MaxX);
                 }
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(X, Y, ZSphere);
         }
     }
 
@@ -383,7 +484,7 @@ public class MLClassification : MonoBehaviour
             // Making Color
             int cptb = 0;
             float Y = 0f;
-            float Z = 0f;
+            float X = 0f;
             if (cpt < NbSphere / 2)
             {
                 cptb = 1;
@@ -392,7 +493,7 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 Y = UnityEngine.Random.Range(0.5f, 15f);
-                Z = UnityEngine.Random.Range(-MaxZ, -0.5f);
+                X = UnityEngine.Random.Range(-MaxX, -0.5f);
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
             }
@@ -400,12 +501,12 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Red;
                 Y = UnityEngine.Random.Range(-15f, -0.5f);
-                Z = UnityEngine.Random.Range(0.5f, MaxZ);
+                X = UnityEngine.Random.Range(0.5f, MaxX);
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
             // Decalage en Z
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(X, Y, ZSphere);
         }
     }
 
@@ -437,7 +538,7 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 Y = UnityEngine.Random.Range(0.5f, 15f);
-                Z = UnityEngine.Random.Range(-MaxZ, -0.5f);
+                Z = UnityEngine.Random.Range(-MaxX, -0.5f);
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
             }
@@ -445,12 +546,12 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Red;
                 Y = UnityEngine.Random.Range(-15f, -0.5f);
-                Z = UnityEngine.Random.Range(0.5f, MaxZ);
+                Z = UnityEngine.Random.Range(0.5f, MaxX);
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
             // Decalage en Z
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(Z, Y, ZSphere);
         }
     }
 
@@ -474,7 +575,7 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 Y = UnityEngine.Random.Range(0.5f, 15f);
-                Z = UnityEngine.Random.Range(-MaxZ, -0.5f);
+                Z = UnityEngine.Random.Range(-MaxX, -0.5f);
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
             }
@@ -482,12 +583,12 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Red;
                 Y = UnityEngine.Random.Range(-15f, -0.5f);
-                Z = UnityEngine.Random.Range(0.5f, MaxZ);
+                Z = UnityEngine.Random.Range(0.5f, MaxX);
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
             // Decalage en Z
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(Z, Y, ZSphere);
         }
     }
 
@@ -541,8 +642,8 @@ public class MLClassification : MonoBehaviour
                 GO.tag = "Green";
             }
             // Decalage en Z
-            float Z = -(MaxZ) + ((float)cpt * 2 * (MaxZ + 1) / 50);
-            GO.transform.position = new Vector3(0f, Y, Z);
+            float Z = -(MaxX) + ((float)cpt * 2 * (MaxX + 1) / 50);
+            GO.transform.position = new Vector3(Z, Y, ZSphere);
         }
     }
 
@@ -568,12 +669,12 @@ public class MLClassification : MonoBehaviour
                 if (cpt % 2 == 0)
                 {
                     Y = 15f;
-                    Z = -MaxZ;
+                    Z = -MaxX;
                 }
                 else
                 {
                     Y = -15f;
-                    Z = MaxZ;
+                    Z = MaxX;
                 }
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
@@ -584,17 +685,17 @@ public class MLClassification : MonoBehaviour
                 if (cpt % 2 == 0)
                 {
                     Y = -15f;
-                    Z = -MaxZ;
+                    Z = -MaxX;
                 }
                 else
                 {
                     Y = 15f;
-                    Z = MaxZ;
+                    Z = MaxX;
                 }
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(Z, Y, ZSphere);
         }
     }
 
@@ -615,8 +716,8 @@ public class MLClassification : MonoBehaviour
                 if (PreColor) GO.GetComponent<Renderer>().material = Blue;
                 if (UnityEngine.Random.Range(0, 2) == 1) Y = UnityEngine.Random.Range(-15f, -14f);
                 else Y = UnityEngine.Random.Range(14f, 15f);
-                if (UnityEngine.Random.Range(0, 2) == 1) Z = UnityEngine.Random.Range(-MaxZ, -MaxZ + 1f);
-                else Z = UnityEngine.Random.Range(MaxZ - 1f, MaxZ);
+                if (UnityEngine.Random.Range(0, 2) == 1) Z = UnityEngine.Random.Range(-MaxX, -MaxX + 1f);
+                else Z = UnityEngine.Random.Range(MaxX - 1f, MaxX);
                 GO.name = "Blue" + cpt + 1;
                 GO.tag = "Blue";
             }
@@ -624,21 +725,21 @@ public class MLClassification : MonoBehaviour
             {
                 if (PreColor) GO.GetComponent<Renderer>().material = Red;
                 Y = UnityEngine.Random.Range(-15f, 15f);
-                Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
-                while (Z < -MaxZ + 1 && (Y < -14f || Y > 14f))
+                Z = UnityEngine.Random.Range(-MaxX, MaxX);
+                while (Z < -MaxX + 1 && (Y < -14f || Y > 14f))
                 {
                     Y = UnityEngine.Random.Range(-15f, 15f);
-                    Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
+                    Z = UnityEngine.Random.Range(-MaxX, MaxX);
                 }
-                while (Z > MaxZ - 1 && (Y < -14f || Y > 14f))
+                while (Z > MaxX - 1 && (Y < -14f || Y > 14f))
                 {
                     Y = UnityEngine.Random.Range(-15f, 15f);
-                    Z = UnityEngine.Random.Range(-MaxZ, MaxZ);
+                    Z = UnityEngine.Random.Range(-MaxX, MaxX);
                 }
                 GO.name = "Red" + cpt + 1;
                 GO.tag = "Red";
             }
-            GO.transform.position = new Vector3(0f, Y, Z);
+            GO.transform.position = new Vector3(Z, Y, ZSphere);
         }
     }
 }
